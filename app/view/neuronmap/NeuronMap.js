97aa065579c23a08514b1d3b7de9d6e506bc2ac4
hanming <beherca@gmail.com>
2012-05-15 00:03:31 +0800

    finish first part, able to connect

--- app/view/neuronmap/NeuronMap.js
+++ app/view/neuronmap/NeuronMap.js
@@ -28,64 +28,10 @@ Ext.define('Brain.Object', {
   constructor : function(args) {
     Ext.apply(this, args);
     this.callParent(args);
+    this.draw();
   },
 
   registerListeners : function() {
-    if (Ext.isEmpty(this.s))
-      return;
-    this.s.on('mouseover', function(sprite) {
-      console.log('mouseover');
-      sprite.setAttributes({
-        fill : 'ffff00',
-        stroke : '00ff00',
-        style : {
-          strokeWidth : 1
-        }
-      }, true);
-      sprite.redraw();
-    });
-    this.s.on('mouseout', function(sprite) {
-      console.log('mouseout');
-      sprite.setAttributes({
-        fill : 'ff0000',
-        stroke : 'none'
-      }, true);
-      sprite.redraw();
-    });
-    this.s.on('click', function() {
-      console.log('click');
-    });
-  },
-
-  draw : function() {
-    if (!Ext.isEmpty(this.drawComp)) {
-      if (Ext.isEmpty(this.s)) {
-        this.s = this.drawComp.surface.add({
-          draggable : true,
-          type : 'path',
-          fill : '#ff0000',
-          radius : 10,
-          x : this.x,
-          y : this.y
-        });
-        this.registerListeners();
-      } else {
-        this.s.setAttributes({
-          x : this.x,
-          y : this.y
-        });
-      }
-      this.s.redraw();
-    }
-  }
-});
-
-Ext.define('Brain.Neuron', {
-  extend : 'Brain.Object',
-
-  constructor : function(args) {
-    Ext.apply(this, args);
-    this.callParent(args);
   },
 
   draw : function() {
@@ -94,9 +40,9 @@ Ext.define('Brain.Neuron', {
       if (Ext.isEmpty(me.s)) {
         me.s = me.drawComp.surface.add({
           draggable : true,
-          type : 'path',
+          type : 'circle',
           fill : '#ff0000',
-          path : [ 'M', me.x, me.y, 'l 20 0 l -10 10 z'].join(' '),
+          radius : 10,
           x : me.x,
           y : me.y
         });
@@ -112,17 +58,93 @@ Ext.define('Brain.Neuron', {
   }
 });
 
+Ext.define('Brain.Neuron', {
+  extend : 'Brain.Object',
+  
+  radius : 20,
+  
+  synapses : [],
+  
+  constructor : function(args) {
+    Ext.apply(this, args);
+    this.callParent(args);
+  },
+
+  draw : function() {
+    var me = this;
+    if (!Ext.isEmpty(me.drawComp)) {
+      if (Ext.isEmpty(me.s)) {
+        me.s = me.drawComp.surface.add({
+          draggable : true,
+          type : 'circle',
+          fill : '#ff0000',
+//          path : [ 'M', me.x - this.width/2, me.y - this.height - 10, 'l',
+//                   this.width, '0 l', -this.width/2, this.height * 0.7, 'z'].join(' '),
+          radius : this.radius,
+          x : me.x,
+          y : me.y
+        });
+        me.registerListeners();
+      } else {
+        me.s.setAttributes({
+          x : me.x,
+          y : me.y
+        });
+      }
+      me.s.redraw();
+    }
+  },
+  
+  registerListeners : function() {
+    var me = this;
+    if (Ext.isEmpty(me.s))
+      return;
+    me.s.on('mouseover', function(sprite) {
+      console.log('mouseover');
+      me.drawComp.focusObjects.push(sprite);
+      sprite.setAttributes({
+        fill : 'ffff00',
+        stroke : '00ff00',
+        style : {
+          strokeWidth : 1
+        }
+      }, true);
+      sprite.redraw();
+    });
+    me.s.on('mouseout', function(sprite) {
+      console.log('mouseout');
+      me.drawComp.focusObjects.pop();
+      sprite.setAttributes({
+        fill : 'ff0000',
+        stroke : 'none'
+      }, true);
+      sprite.redraw();
+    });
+    me.s.on('click', function(sprite) {
+      console.log('click');
+      var neuronMap = me.drawComp.neuronMap;
+      neuronMap.registerNeuron(me);
+    });
+  }
+});
+
 Ext.define('Brain.Synapse', {
   extend : 'Brain.Object',
   dot : null,
 
-  end : {
-    x : 0,
-    y : 0
-  },
+  preNeuron : null,
+  
+  postNeuron : null,
+  
+  endX : 0,
+  endY : 0,
 
   constructor : function(args) {
     Ext.apply(this, args);
+    this.x = this.preNeuron ? this.preNeuron.x: this.x;
+    this.y = this.preNeuron ? this.preNeuron.y: this.y;
+    this.endX = this.postNeuron ? this.postNeuron.x : this.endX;
+    this.endY = this.postNeuron ? this.postNeuron.y: this.endY;
     this.callParent(args);
   },
 
@@ -135,7 +157,7 @@ Ext.define('Brain.Synapse', {
           type : 'path',
           fill : 'none',
           stroke : 'blue',
-          path : [ 'M', me.x, me.y, 'l', me.end.x, me.end.y ].join(' '),
+          path : [ 'M', me.x, me.y, 'L', me.endX, me.endY ].join(' '),
           x : me.x,
           y : me.y
         });
@@ -144,8 +166,8 @@ Ext.define('Brain.Synapse', {
           fill : me.isReverse ? '#ff' : '#ffffff',
           stroke : 'blue',
           radius : 10,
-          x : me.x + (me.end.x - me.x) / 2,
-          y : me.y + (me.end.y - me.y) / 2
+          x : (me.x + me.endX) / 2,
+          y : (me.y + me.endY) / 2
         });
         // this.registerListeners();
       } else {
@@ -169,15 +191,20 @@ Ext.define('AM.view.neuronmap.NeuronMap', {
   id : 'neuron-map',
   extend : 'Ext.container.Container',
   alias : 'widget.neuronmap',
+  
   layout : {
     type : 'border',
     border : 2
   },
-
+  
   mode : MODE.NORMAL,
+  
+  /**This is the neurons to be connected */
+  preNeuron : null,
 
   items : [ {
     xtype : 'toolbar',
+    itemId : 'brainMapMenu',
     items : [ {
       iconCls : 'neuron-active-btn',
       id : 'neuron-active-btn',
@@ -217,6 +244,11 @@ Ext.define('AM.view.neuronmap.NeuronMap', {
     region : 'center',
     itemId : 'drawpanel',
     viewBox : false,
+    /**this is the array to store the object that get the focus usually, only one get focuse,
+     * when it get focus, this can prevent user from adding neuron when the mouse cusor is on 
+     * existing another neuron*/
+    focusObjects : [],
+    neuronMap : null,
     items : [ {
       type : 'rect',
       fill : '#79BB3F',
@@ -238,38 +270,65 @@ Ext.define('AM.view.neuronmap.NeuronMap', {
     console.log('view ok');
     me.callParent(arguments);
     var drawpanel = me.getComponent('drawpanel');
+    drawpanel.neuronMap = me;
     drawpanel.on('click', function(e, t, opts) {
-      console.log('hello dome');
-      if (me.mode == MODE.NEURON) {
-        me.addNeuron(drawpanel, e.getXY());
+      console.log('draw panel click');
+      if(drawpanel.focusObjects.length > 0){
+        
+      }else {
+        if (me.mode == MODE.NEURON) {
+          var offset = me.getComponent('brainMapMenu').getHeight() + me.layout.border;
+          me.addNeuron(drawpanel, e.getXY(), offset);
+        }else{
+          me.preNeuron = null;
+        }
       }
     });
     var bno = Ext.create('Brain.Neuron', {
       drawComp : drawpanel,
-      x : 20,
-      y : 20
+      x : 40,
+      y : 40
     });
-    bno.draw();
-    bno.x = 300;
-    bno.draw();
+//    bno.draw();
     var syn = Ext.create('Brain.Synapse', {
       drawComp : me.getComponent('drawpanel'),
       x : 10,
       y : 10,
-      end : {
-        x : 100,
-        y : 100
-      }
+      endX : 100,
+      endY : 100
     });
-    syn.draw();
+//    syn.draw();
   },
 
-  addNeuron : function(drawpanel, xy) {
+  addNeuron : function(drawpanel, xy, offset) {
     var bno = Ext.create('Brain.Neuron', {
       drawComp : drawpanel,
       x : xy[0],
-      y : xy[1] -20
+      y : xy[1] - offset
     });
-    bno.draw();
+    return bno;
+  },
+  
+  addSynapse : function(preNeuron, postNeuron){
+    var syn = Ext.create('Brain.Synapse', {
+      drawComp : this.getComponent('drawpanel'),
+      preNeuron : preNeuron,
+      postNeuron : postNeuron
+    });
+    return syn;
+  },
+  
+  registerNeuron : function(neuron){
+    var me = this;
+    if(me && me.mode == MODE.SYNAPSE){
+      if(me.preNeuron && me.preNeuron != neuron){// that means already has first neuron checked, this is the 2nd
+        var synapse = me.addSynapse(me.preNeuron, neuron);
+        me.preNeuron.synapses.push(synapse);
+        neuron.synapses.push(synapse);
+        me.preNeuron = null;
+      }else{
+        me.preNeuron = neuron;
+      }
+    }
   }
 });
