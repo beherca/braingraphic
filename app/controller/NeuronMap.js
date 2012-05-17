756bea91a8c4ebddcd9a17cbe64bb50cd08597e0
hanming <beherca@gmail.com>
2012-05-17 11:00:36 +0800

    add controller for future storage

--- app/controller/NeuronMap.js
+++ app/controller/NeuronMap.js
@@ -1,7 +1,22 @@
 Ext.define('AM.controller.NeuronMap', {
-    extend: 'Ext.app.Controller',
-    view : [{xtype : 'neuronmap'}],
-    init: function() {
-      console.log('NeuronMap Controller Start');
+  extend : 'Ext.app.Controller',
+  view : [ {
+    xtype : 'neuronmap'
+  } ],
+  init : function() {
+    console.log('NeuronMap Controller Start');
+    this.control({
+      'container > toolbar>button[action=save]' : {
+        click : this.saveMap
+      }
+    });
   },
+  
+  saveMap : function(btn){
+    console.log('saving map');
+    var me = this;
+    var nm = btn.up('neuronmap');
+    var json = Ext.JSON.encode(nm.neurons);
+    console.log(json);
+  }
 });
