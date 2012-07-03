c4c2f768f4d8f21fcd9a647c6edd9b5f343f2f23
hanming <beherca@gmail.com>
2012-07-03 19:42:41 +0800

    add the core.js for future node to use

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ sencha build -p app.jsb3 -d ./
 target='../brainmapstorage/public'
 cp -f index_production.html $target/index.html
 cp -f app-all.js $target
+cp -f core.js $target
 cp -rf app $target
 cp -rf images $target
 cp -rf stylesheets $target
