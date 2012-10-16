b61d8236311a43748fd9aea4009b6941d4f31da4
hanming <beherca@gmail.com>
2012-10-16 15:14:16 +0800

    change comments only

--- core/brain/brain.js
+++ core/brain/brain.js
@@ -1,21 +1,14 @@
-/*
+/**
+* @Author : Kai Li
+* Copyright (c) 2012 Kai Li
+* Contact:  beherca@gmail.com
+* This work is to simulate the nature brain 
+* All Right Reserved
+*/
 
-@Author : Kai Li
-
-Copyright (c) 2012 Kai Li
-
-Contact:  http://weibo.com/beherca
-
-This work is to simulate the nature brain 
-
-GNU General Public License Usage
-This file may be used under the terms of the GNU General Public License version 3.0 as published by the Free Software Foundation and appearing in the file LICENSE included in the packaging of this file.  Please review the following information to ensure the GNU General Public License version 3.0 requirements will be met: http://www.gnu.org/copyleft/gpl.html.
-
+/**
+ * global g_accuracy, back to 6 decimal
  */
-
-  /**
-   * global g_accuracy, back to 6 decimal
-   */
 g_accuracy = 6;
   
 /**
