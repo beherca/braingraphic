1ed143c722a3863e1a7c5471cef5e17d98dc99a3
hanming <beherca@gmail.com>
2012-10-20 13:06:29 +0800

    add test case to verify inheritance correct

--- test/018_class_getter_setter.t.js
+++ test/018_class_getter_setter.t.js
@@ -16,6 +16,7 @@ StartTest(function(t) {
 
   
   var C = cls.extend(Observable, {
+    alias : 'C',
     value : 3,
     testC : function(){
       return 'I am C';
@@ -28,29 +29,56 @@ StartTest(function(t) {
       this.value = v;
     }
   });
+//  
+//  var D = cls.extend(Observable, {
+//    alias : 'D',
+//    value : 4,
+//    get$c : function(){
+//      return this.value;
+//    }
+//  });
   
-  var D = cls.extend(Observable, {
-    value : 4,
-    get$c : function(){
-      return this.value;
+  var A = cls.extend(C, {
+    alias : 'A_C',
+    avalue : 4,
+    get$a : function(){
+      return this.avalue;
     }
   });
-
-  var c = cls.create(C);
-  console.log(c);
-  t.is(Utils.isFunction(c.__lookupGetter__('c')), true, 'C contains getter');
-  t.is(Utils.isFunction(c.__lookupSetter__('c')), true, 'C contains setter');
-  //set value to 4
-  c.c = 4;
-  t.is(c.c, 4, 'Value is set successfully');
+//
+//  t.diag('Test getter setter normal');
+//  var c = cls.create(C);
+//  console.log(c);
+//  t.is(Utils.isFunction(c.__lookupGetter__('c')), true, 'C contains getter');
+//  t.is(Utils.isFunction(c.__lookupSetter__('c')), true, 'C contains setter');
+//  //set value to 4
+//  c.c = 4;
+//  t.is(c.c, 4, 'Value is set successfully');
+//  
+//  t.diag('Test getter setter without setter');
+//  var d = cls.create(D);
+//  console.log(d);
+//  t.is(Utils.isFunction(d.__lookupGetter__('c')), true, 'D contains getter');
+//  t.is(Utils.isFunction(d.__lookupSetter__('c')), false, 'D does not contain setter');
+//  t.is(d.c, 4, 'D\' value is 4');
+//  d.c = 5;
+//  t.is(d.c, 4, 'D\' value is still 4');
+//  
+//  t.diag('Test inheritance again');
+  var a = cls.create(A);
+  console.log(a);
+  t.is(Utils.isFunction(a.__lookupGetter__('c')), true, 'A contains getter from C');
+  t.is(Utils.isFunction(a.__lookupSetter__('c')), true, 'A contains setter from C');
+  t.is(Utils.isFunction(a.__lookupGetter__('a')), true, 'A contains setter from A');
+  t.is(Utils.isFunction(a.__lookupSetter__('a')), false, 'A does not contain setter from A');
   
-  var d = cls.create(D);
-  console.log(d);
-  t.is(Utils.isFunction(d.__lookupGetter__('c')), true, 'D contains getter');
-  t.is(Utils.isFunction(d.__lookupSetter__('c')), false, 'D does not contain setter');
-  t.is(d.c, 4, 'D\' value is 4');
-  d.c = 5;
-  t.is(d.c, 4, 'D\' value is still 4');
+  t.is(a.c, 3, 'A\' value c is 3');
+  a.c = 4;
+  t.is(a.c, 4, 'A\' value c is changes to 4');
+  
+  t.is(a.a, 4, 'A\' value a is 4');
+  a.a = 5;
+  t.is(a.a, 4, 'A\' value a is still 4');
   
   t.done(); // Optional, marks the correct exit point from the test
 });
