ab1b51731042c1714efb772a769e0a135c11381e
hanming <beherca@gmail.com>
2012-10-20 18:49:51 +0800

    expose Iider to node

--- core/mconf.js
+++ core/mconf.js
@@ -5,6 +5,7 @@
     module.exports.Brain = Brain;
     module.exports.Observable = Observable;
     module.exports.Iid = Iid;
+    module.exports.Iider = Iider;
     module.exports.Indexer = Indexer;
     module.exports.Looper = Looper;
   }
